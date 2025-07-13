69e7bc247b811e2a8ef8f04792bbe9e5451b3404
pylixonly <82711525+pylixonly@users.noreply.github.com>
2025-07-13 14:26:26 +0800

    chore: bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "dev.wintry.xposed"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1001
-        versionName = "1.0.1"
+        versionCode = 1002
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
